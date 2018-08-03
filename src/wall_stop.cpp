2f239b714d7865a3720bdadd462e44310460a38b
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-08-03 16:28:10 +0900

    Make wall_stop pass the test

--- src/wall_stop.cpp
+++ src/wall_stop.cpp
@@ -13,10 +13,11 @@ using namespace ros;
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php */
 
+int sensor_sum_all = 0;
 
 void callback(const raspimouse_ros_2::LightSensorValues::ConstPtr& msg)
 {
-
+	sensor_sum_all = msg->sum_all;
 }
 
 void onSigint(int sig)
@@ -26,8 +27,32 @@ void onSigint(int sig)
 	shutdown();
 }
 
-void run(void)
+void run(Publisher *pub)
 {
+	geometry_msgs::Twist tw;
+	tw.linear.x = 0.2;
+	tw.angular.z = 0.0;
+
+	if(sensor_sum_all >= 500){
+		tw.linear.x = 0.0;
+	}
+	pub->publish(tw);
+/*
+class WallTrace():
+    def run(self):
+        rate = rospy.Rate(10)
+        data = Twist()
+
+        while not rospy.is_shutdown():
+            data.linear.x = 0.2
+            data.angular.z = 0
+            if self.sensor_values.sum_all >= 500:
+                data.linear.x = 0
+                data.angular.z = 0
+
+            self.cmd_vel.publish(data)
+            rate.sleep()
+    */
 }
 
 int main(int argc, char **argv)
@@ -53,53 +78,10 @@ int main(int argc, char **argv)
 
 	unsigned int c = 0;
 	while(ok()){
-		run();
-		/*
-		std::ifstream ifs("/dev/rtlightsensor0");
-		ifs >> msg.right_forward >> msg.right_side
-			>> msg.left_side >> msg.left_forward;
-
-		msg.sum_forward = msg.left_forward + msg.right_forward;
-		msg.sum_all = msg.sum_forward + msg.left_side + msg.right_side;
-
-		pub.publish(msg);
-		*/
+		run(&pub);
 		spinOnce();
 		loop_rate.sleep();
 	}
 	exit(0);
 }
 
-/*
-
-
-class WallTrace():
-    def __init__(self):
-        self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
-
-        self.sensor_values = LightSensorValues()
-        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_lightsensors)
-
-    def callback_lightsensors(self,messages):
-        self.sensor_values = messages
-
-    def run(self):
-        rate = rospy.Rate(10)
-        data = Twist()
-
-        while not rospy.is_shutdown():
-            data.linear.x = 0.2
-            data.angular.z = 0
-            if self.sensor_values.sum_all >= 500:
-                data.linear.x = 0
-                data.angular.z = 0
-
-            self.cmd_vel.publish(data)
-            rate.sleep()
-
-if __name__ == '__main__':
-    rospy.init_node('wall_trace')
-
-    w = WallTrace()
-    w.run()
-    */
