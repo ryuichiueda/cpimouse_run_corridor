1ce1f1f743705fd9e45b5ab4b83384eafc66897d
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-08-09 17:04:47 +0900

    Change dir name

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -18,7 +18,7 @@ sudo apt install ros-kinetic-tf2
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
