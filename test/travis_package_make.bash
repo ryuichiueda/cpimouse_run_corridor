1b04fbcf42c209145d46202de5627a5454f5e13d
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-08-09 17:09:37 +0900

    Fix old description

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,12 +1,9 @@
 #!/bin/bash -xve
 
-#ワークスペースにリポジトリをコピー
-rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+rsync -av ./ ~/catkin_ws/src/cpimouse_run_corridor/
 
-#pimouse_rosをgit cloneでワークスペースに持ってくる
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/citueda/pimouse_ros.git
-        #↑depth=1を指定すると最新のものだけクローンできる
+git clone --depth=1 https://github.com/citueda/raspimouse_ros_2.git
 
 cd ~/catkin_ws
 catkin_make
