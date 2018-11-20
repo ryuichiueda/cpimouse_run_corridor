00b6fbd12136aac30ea4a43ed6c63e5cfeb22d46
Ryuichi Ueda <ryuichiueda@gmail.com>
2018-11-20 10:58:27 +0900

    Make the time interval long

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -9,7 +9,7 @@ class WallStopTest(unittest.TestCase):
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
             f.flush()
 
-        time.sleep(0.3)
+        time.sleep(1.0)
 
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
